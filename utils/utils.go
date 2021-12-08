550d1e8db89ce27a809aea222b6d00217594717b
Andreo <andreobouzas@gmail.com>
2021-12-08 18:11:31 -0300

    Adição dos comentários ao código

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,15 @@
 package Utils
 
+//Import do Pkg Regexp
 import "regexp"
 
+//Criação da função de procura no texto baseada no regexp
+//Definie-se que a função receberá um alvo, um padrão de regexp
 func FindInText(target, patern string) [][]string {
+	//definindo o padrão informado como base de procura no texto
 	paterntext := regexp.MustCompile(patern)
-	//cleartext := paterntext.FindAllString(target, -1)
+	//passando o regexp no texto e salvando o resultado em uma variável
 	cleartext := paterntext.FindAllStringSubmatch(target, -1)
+	//Definindo o retorno da função
 	return cleartext
 }
